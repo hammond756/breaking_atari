408d3b0d571cf698ca876f6a4556ac072e43b1a3
aronhammond <hammond756@live.nl>
2019-02-09 10:37:52 +0100

    read height/width from observation space, not directly from properties

--- breaking_atari/atari_wrappers/annotate_wrapper.py
+++ breaking_atari/atari_wrappers/annotate_wrapper.py
@@ -14,8 +14,10 @@ class AnnotateFrame(gym.ObservationWrapper):
         self.templates = templates
         self.threshold=threshold
 
+        height, width = env.observation_space.shape[0:2]
+
         self.observation_space = gym.spaces.Box(low=0, high=255,
-                shape=(env.height, env.width, 3), dtype=np.uint8)
+                shape=(height, width, 3), dtype=np.uint8)
 
     def observation(self, frame):
         observation = mask(self.templates, frame, self.threshold)
